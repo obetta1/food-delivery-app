0c9cbb7b3db6a65ec18be61b2e01debd01a2eba6
obetta1 <obettabenjamin44@gmail.com>
2023-06-06 11:12:13 +0100

    project settup

---
+++ android/app/src/main/kotlin/com/ben/food_delivary_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ben.food_delivary_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
